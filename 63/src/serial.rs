1190cc573173d0984431efc20126d0413fd979ad
Liam Cochran <liamcochran55@gmail.com>
2026-04-03 14:43:51 -0700

    idk

---
+++ 63/src/serial.rs
@@ -0,0 +1,29 @@
+use core::fmt;
+use uart_16550::SerialPort;
+
+/// Prints to the host through the serial interface.
+#[macro_export]
+macro_rules! serial_print {
+    ($($arg:tt)*) => {
+        $crate::serial::_serial_print(format_args!($($arg)*));
+    };
+}
+
+/// Prints to the host through the serial interface, appending a newline.
+#[macro_export]
+macro_rules! serial_println {
+    () => ($crate::serial_print!("\n"));
+    ($fmt:expr) => ($crate::serial_print!(concat!($fmt, "\n")));
+    ($fmt:expr, $($arg:tt)*) => ($crate::serial_print!(
+        concat!($fmt, "\n"), $($arg)*));
+}
+
+#[doc(hidden)]
+pub fn _serial_print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    use x86_64::instructions::interrupts;
+
+    interrupts::without_interrupts(|| {
+        serial_println!("{}", args);
+    });
+}
---
+++ 63/tests/println.rs
@@ -0,0 +1,38 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(binkle_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use binkle_os::{print, println};
+
+#[panic_handler]
+fn test_panic(_info: &core::panic::PanicInfo) -> ! {
+    binkle_os::serial_println!("[Pass]");
+    binkle_os::exit_qemu(binkle_os::QEMU_PASS);
+    binkle_os::hlt_loop();
+}
+
+#[test_case]
+fn test_println() {
+    println!("Binkle is the most epic cat boy");
+}
+
+#[test_case]
+fn test_wrapping_println() {
+    for _ in 0..534 {
+        print!("Binkle");
+    }
+}
+
+#[test_case]
+fn good() {
+    assert!(true);
+}
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    binkle_os::exit_qemu(binkle_os::QEMU_FAIL);
+    binkle_os::hlt_loop();
+}
